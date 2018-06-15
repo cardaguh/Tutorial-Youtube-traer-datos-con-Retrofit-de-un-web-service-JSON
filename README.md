3e3180feedc29ec914dda694009696062958711b
cardaguh <cardaguh@gmail.com>
2018-06-15 12:09:17 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+
+Tutorial del uso de Retrofit para traer datos de un web service o API a un recyclerview desde una aplicación Android.
---
+++ app/src/main/java/com/jikansoftware/retrofitdemostracion/model/Contact.java
@@ -0,0 +1,20 @@
+package com.jikansoftware.retrofitdemostracion.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Contact {
+
+    @SerializedName("name")
+    private String Name;
+
+    @SerializedName("email")
+    private String Email;
+
+    public String getName() {
+        return Name;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+}
